b8a60fcd172981e7b2b9e3b37b52d31bfdcc647e
jdemmith <jdemmith@student.neumont.edu>
2013-07-30 23:06:59 -0600

    ws_lunch
    REST client and server

---
+++ ws_lunch/JDemmith_REST/src/servlets/Item.java
@@ -0,0 +1,13 @@
+package servlets;
+
+public class Item {
+
+	double price;
+	String name;
+	
+	public Item(String name, double price) {
+		this.name=name;
+		this.price=price;
+	}
+
+}
---
+++ ws_lunch/JDemmith_REST/src/servlets/Menu.java
@@ -0,0 +1,13 @@
+package servlets;
+import java.util.List;
+
+
+public class Menu {
+
+	List<Item>items;
+	
+	public Menu(List<Item> items) {
+		this.items = items;
+	}
+
+}
